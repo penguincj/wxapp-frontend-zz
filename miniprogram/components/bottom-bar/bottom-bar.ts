ce7f9434a06514c82548d022b563bdf7609ffc72
wye <muzigogo@qq.com>
2025-02-16 12:10:04 +0800

    bottom bar

--- miniprogram/components/bottom-bar/bottom-bar.ts
+++ miniprogram/components/bottom-bar/bottom-bar.ts
@@ -1,10 +1,11 @@
+import { getCurrentPageUrl, getCurrentPageParam } from "../../utils/util";
 const itemConfig = [
   {
     id: 0,
     name: '首页',
     icon: '/static/images/ishouye.png',
     iconClicked: '/static/images/ishouye-c.png',
-    link: 'pages/index/index',
+    link: '/pages/index/index',
   },
   // {
   //   id: 1,
@@ -18,14 +19,14 @@ const itemConfig = [
     name: '展览',
     icon: '/static/images/izhanlan.png',
     iconClicked: '/static/images/izhanlan-c.png',
-    link: 'pages/index/index',
+    link: '/pages/exhibitionlist/index',
   },
   {
     id: 2,
     name: '我的',
     icon: '/static/images/iwode.png',
     iconClicked: '/static/images/iwode-c.png',
-    link: 'pages/index/index',
+    link: '/pages/profile/index',
   },
 ]
 Component({
@@ -46,16 +47,43 @@ Component({
    */
   data: {
     icons: itemConfig,
-  },
-  lifetimes: {
-    attached() {
-      
-    },
+    selectid: 0,
+    urlParams: {},
   },
   /**
    * 组件的方法列表
    */
   methods: {
-  
+    handleClickBottomBar(e: any) {
+      const {idx} =e.currentTarget.dataset;
+      
+      const item = itemConfig.find(i => i.id === idx);
+      // this.setData({
+      //   selectid: idx,
+      // });
+      console.log('current idx', idx, item);
+
+      if (item && item.link) {
+        wx.redirectTo({
+          url: item.link,
+          fail(error) {
+            console.log(error)
+          }
+        })
+      }
+    },
+
   },
+ 
+
+  lifetimes: {
+    attached() {
+      setTimeout(() => {
+        const url = getCurrentPageUrl();
+        const param = getCurrentPageParam();
+        console.log('current url', url, param);
+      })
+    },
+  },
+  
 })
--- miniprogram/utils/util.ts
+++ miniprogram/utils/util.ts
@@ -41,3 +41,18 @@ export function throttle(fn: any, interval: number) {
   };
 }
 
+
+/*获取当前页url*/
+export const getCurrentPageUrl=()=>{
+  let pages = getCurrentPages()    //获取加载的页面
+  let currentPage = pages[pages.length-1]    //获取当前页面的对象
+  let url = currentPage.route    //当前页面url
+  return url
+}
+/*获取当前页参数*/
+export const getCurrentPageParam=()=>{
+  let pages = getCurrentPages()    //获取加载的页面
+  let currentPage = pages[pages.length-1]    //获取当前页面的对象
+  let options = currentPage.options;    //如果要获取url中所带的参数可以查看options
+  return options;
+}
