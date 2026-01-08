eaed3f4e7a5aa7e314045ca814ad999dbcc3c2ba
格格 <muzigogo@qq.com>
2026-01-08 21:16:09 +0800

    p2

---
+++ miniprogram/pages/treature-ranklist/index.ts
@@ -0,0 +1,58 @@
+Page({
+  data: {
+    topRows: [
+      [
+        "https://gewugo.com/api/v1/storage/image/share-3639793484.jpg",
+        "https://gewugo.com/api/v1/storage/image/Frame8@1x-3092466763.webp",
+        "https://gewugo.com/api/v1/storage/image/Union@1x-6352490503.webp",
+        "https://gewugo.com/api/v1/storage/image/share-3639793484.jpg",
+        "https://gewugo.com/api/v1/storage/image/Frame8@1x-3092466763.webp",
+        "https://gewugo.com/api/v1/storage/image/Union@1x-6352490503.webp",
+        "https://gewugo.com/api/v1/storage/image/share-3639793484.jpg",
+        "https://gewugo.com/api/v1/storage/image/Frame8@1x-3092466763.webp"
+      ],
+      [
+        "https://gewugo.com/api/v1/storage/image/Union@1x-6352490503.webp",
+        "https://gewugo.com/api/v1/storage/image/share-3639793484.jpg",
+        "https://gewugo.com/api/v1/storage/image/Frame8@1x-3092466763.webp",
+        "https://gewugo.com/api/v1/storage/image/Union@1x-6352490503.webp",
+        "https://gewugo.com/api/v1/storage/image/share-3639793484.jpg",
+        "https://gewugo.com/api/v1/storage/image/Frame8@1x-3092466763.webp",
+        "https://gewugo.com/api/v1/storage/image/Union@1x-6352490503.webp",
+        "https://gewugo.com/api/v1/storage/image/share-3639793484.jpg"
+      ]
+    ],
+    rankList: [
+      {
+        id: 1,
+        name: "文物名称",
+        location: "展厅·单元",
+        image: "https://gewugo.com/api/v1/storage/image/share-3639793484.jpg"
+      },
+      {
+        id: 2,
+        name: "文物名称",
+        location: "展厅·单元",
+        image: "https://gewugo.com/api/v1/storage/image/share-3639793484.jpg"
+      },
+      {
+        id: 3,
+        name: "文物名称",
+        location: "展厅·单元",
+        image: "https://gewugo.com/api/v1/storage/image/share-3639793484.jpg"
+      },
+      {
+        id: 4,
+        name: "文物名称",
+        location: "展厅·单元",
+        image: "https://gewugo.com/api/v1/storage/image/share-3639793484.jpg"
+      },
+      {
+        id: 5,
+        name: "文物名称",
+        location: "展厅·单元",
+        image: "https://gewugo.com/api/v1/storage/image/share-3639793484.jpg"
+      }
+    ]
+  }
+});
